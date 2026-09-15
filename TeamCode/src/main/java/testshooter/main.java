38f0dc35cdea4847ab7e45cb6c75ed448a73a7da
Moses Tang <tangyanchimoses@gmail.com>
2026-09-15 16:59:55 +0800

    adfad

---
+++ TeamCode/src/main/java/testshooter/main.java
@@ -0,0 +1,12 @@
+package testshooter;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp
+public class main {
+
+
+
+
+}
